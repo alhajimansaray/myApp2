33386906fa08fed459f63e7954d4c30ed5832fe4
alhajimansaray <alhaji.mansaray.ubuntuafrika@gmail.com>
2021-06-25 12:02:38 +0000

    Default Changelist

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template:`
+<!--  <p>This is a warning, you're in danger!</p>-->
+  `,
+  styles: [
+    `
+    /*p{
+      padding: 20px;
+      background-color: rosybrown ;
+      border: 1px solid red;
+    }*/
+    `
+  ],
+
+})
+export class WarningAlertComponent {
+
+}
